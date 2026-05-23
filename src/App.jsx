f8c441a03f7bb63fdf705d8c65d04b33ef77648a
erlbpvtltd <erlbpvtltd@gmail.com>
2026-05-23 10:36:43 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -11,7 +11,7 @@ import 'react-toastify/dist/ReactToastify.css';
 
 const App = () => {
 
-  const backendUrl = 'http://localhost:4000';
+  const backendUrl = 'https://backend-psbv.onrender.com';
 
   return (
     <>
